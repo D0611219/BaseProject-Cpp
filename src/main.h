f321eace0af7101d39c45113b7aec352455b5afd
D0611219 <kevin50605123@yahoo.com.tw>
2018-10-08 20:16:47 +0800

    123

--- src/main.h
+++ src/main.h
@@ -3,7 +3,11 @@
 
 int addNumber(int a, int b);
 int getTriangleType(int a, int b, int c);
+<<<<<<< Updated upstream
 int jinjin(int a, int b);
 void gay();
 
+=======
+int minusNumber(int a, int b);
+>>>>>>> Stashed changes
 #endif // MAIN_H_INCLUDED
